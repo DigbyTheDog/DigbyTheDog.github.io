5da330caa19f00b12e0ec2f24d040b7a7a9ad240
DigbyTheDog <leescha@mail.gvsu.edu>
2020-01-14 21:25:41 -0500

    started pagination

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@
 
   	/* Mobile Menu
    ------------------------------------------------------ */  
-  	var toggle_button = $("<a>", {                         
-                        id: "toggle-btn", 
-                        html : "Menu",
-                        title: "Menu",
-                        href : "#" } 
-                        );
-  	var nav_wrap = $('nav#nav-wrap')
-  	var nav = $("ul#nav");  
+   var toggle_button = $("<a>", {                         
+    id: "toggle-btn", 
+    html : "Menu",
+    title: "Menu",
+    href : "#" } 
+    );
+   var nav_wrap = $('nav#nav-wrap')
+   var nav = $("ul#nav");  
 
   	/* if JS is enabled, remove the two a.mobile-btns 
   	and dynamically prepend a.toggle-btn to #nav-wrap */
@@ -24,56 +24,57 @@
   	nav_wrap.prepend(toggle_button); 
 
   	toggle_button.on("click", function(e) {
-   	e.preventDefault();
-    	nav.slideToggle("fast");     
-  	});
+      e.preventDefault();
+      nav.slideToggle("fast");     
+    });
 
   	if (toggle_button.is(':visible')) nav.addClass('mobile');
   	$(window).resize(function(){
-   	if (toggle_button.is(':visible')) nav.addClass('mobile');
-    	else nav.removeClass('mobile');
-  	});
+      if (toggle_button.is(':visible')) nav.addClass('mobile');
+      else nav.removeClass('mobile');
+    });
 
   	$('ul#nav li a').on("click", function(){      
-   	if (nav.hasClass('mobile')) nav.fadeOut('fast');      
-  	});	
+      if (nav.hasClass('mobile')) nav.fadeOut('fast');      
+    });	
 
-  
+
   	/* Smooth Scrolling
   	------------------------------------------------------ */
   	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
 
-   	var target = this.hash,
-    	$target = $(target);
+     e.preventDefault();
 
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
+     var target = this.hash,
+     $target = $(target);
 
-  	}); 
+     $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 800, 'swing', function () {
+     window.location.hash = target;
+   });
+
+   }); 
 
   	
   	/*	Back To Top Button
-	------------------------------------------------------- */
+   ------------------------------------------------------- */
 	var pxShow = 300; //height on which the button will show
 	var fadeInTime = 400; //how slow/fast you want the button to show
 	var fadeOutTime = 400; //how slow/fast you want the button to hide
 	var scrollSpeed = 300; //how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
    // Show or hide the sticky footer button
-	$(window).scroll(function() {
+   $(window).scroll(function() {
 
-		if ($(window).scrollTop() >= pxShow) {
-			$("#go-top").fadeIn(fadeInTime);
-		} else {
-			$("#go-top").fadeOut(fadeOutTime);
-		}
+    if ($(window).scrollTop() >= pxShow) {
+     $("#go-top").fadeIn(fadeInTime);
+   } else {
+     $("#go-top").fadeOut(fadeOutTime);
+   }
 
-	});  
+ });  
 
 
-})(jQuery);
+ })(jQuery);
+
